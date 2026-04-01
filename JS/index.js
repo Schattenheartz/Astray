49af04a4fdc04c3f42ac318f2d88c540d3c4658e
Schattenheartz <aquice2001@gmail.com>
2026-04-01 17:47:58 -0500

    fix: patch layout bugs in HTML and CSS files

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,20 @@
-/* Show Menu */
+/* Referencias a los elementos del DOM (Document Object Model) */
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/* Menu Show */
+/* Abrir el menu al hacer clic en el boton toggle */
 if(navToggle){
    navToggle.addEventListener('click', () =>{
+      /* Agrega la clase CSS que mueve el menu hacia adentro de la pantalla */
       navMenu.classList.add('show-menu')
    })
 }
 
-/* Menu Hidden */
+/* Cerrar el menu al hacer clic en el icono de la X */
 if(navClose){
    navClose.addEventListener('click', () =>{
+      /* Elimina la clase CSS para ocultar el menu nuevamente */
       navMenu.classList.remove('show-menu')
    })
 }
